6b9b0023d4f067280b07415224b9d0f5041d7262
Esteban Flores <e2002florespulido@gmail.com>
2024-12-27 10:35:02 -0600

    Modifying package json

--- index.js
+++ index.js
@@ -20,10 +20,7 @@ cloudinary.config({
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
