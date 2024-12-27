f74e87de7a68b0097fc6526639f1516012b5efbf
Esteban Flores <e2002florespulido@gmail.com>
2024-12-27 10:27:54 -0600

    Modifying package json

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import path from "path"
 import http from "http"
 import { v2 as cloudinary } from "cloudinary"
 
@@ -20,7 +19,6 @@ cloudinary.config({
 
 const app = express()
 const PORT = process.env.PORT || 5000
-const __dirname = path.resolve()
 
 app.use(cors())
 app.use(express.json())
