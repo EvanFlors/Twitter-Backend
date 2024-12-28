ae18696a1b182dab653001c13167c03d85590721
Esteban Flores <e2002florespulido@gmail.com>
2024-12-27 19:41:53 -0600

    First commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -69,10 +69,9 @@ export const login = async (req, res) => {
 			return res.status(400).json({ error: "Invalid username or password" });
 		}
 
-		token = generateTokenAndSetCookie(user._id, res);
+		generateTokenAndSetCookie(user._id, res);
 
 		res.status(200).json({
-      token,
 			_id: user._id,
 			fullName: user.fullName,
 			username: user.username,
