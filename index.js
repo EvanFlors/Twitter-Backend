c90d0874188c7c614d587eed8ccc66543a4886ef
Esteban Flores <e2002florespulido@gmail.com>
2024-12-27 20:06:18 -0600

    First commit

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ const PORT = process.env.PORT || 5000
 
 const corsOptions = {
   origin: "http://localhost:5173",
-  credentials: true,  
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true, 
 };
 
 app.use(cors(corsOptions))
--- src/lib/utils/generateToken.js
+++ src/lib/utils/generateToken.js
@@ -6,10 +6,10 @@ export const generateTokenAndSetCookie = (userId, res) => {
 	})
 
 	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, //MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "lax", // CSRF attacks cross-site request forgery attacks
-		secure: false,
+		maxAge: 15 * 24 * 60 * 60 * 1000, 
+		httpOnly: true, 
+		sameSite: "none", 
+    path: "/"
 	})
 
   return token
